70c2648cf3bd988fcd5dd65d8db1a8d1b00be086
Jose Ignacio Hidalgo <HidalgoJ.Ignacio@gmail.com>
2018-07-02 01:11:17 +0200

    menus navegacion

--- index.php
+++ index.php
@@ -11,9 +11,50 @@
         <script src="main.js"></script>
     </head>
 <body>
-    <div id="DivLogin">
-        <label>Usuario</label><input type="text">
-        <label>Contraseña</label><input type="password">
+    <header>
+        <div id="DivHeader">
+
+        </div>
+    </header>
+
+    <div id="DivNavSup">
+        <nav id="menu_cabecera">
+			<ul>
+				<li><a href="https://www.planetadelibros.com/editorial/editorial-planeta/8">HOME</a></li>
+				<li><a href="#">CATEGORIAS</a>
+					<ul>
+						<li><a href="#">AUTORES</a></li>
+						<li><a href="https://www.planetadelibros.com/editorial/editorial-planeta/autores/a/8">TITULOS</a></li>
+						<li><a href="http://novalibros.com/novedad-editorial/">NOVEDADES</a></li>
+					</ul>
+				</li>
+			    <li><a href="https://es-es.facebook.com/">AMIGOS</a></li>
+			 </ul>
+		</nav>
     </div>
+    <!--seccion de navegación lateral-->
+	<div id="capitulos">
+		<nav>
+			<ul>
+				<li><a href="NoAmigable.html">TITULO LIBRO</a></li>
+				<li><a href="Capitulo1.html">Capitulo 1</a></li>
+				<li><a href="Capitulo2.html">Capitulo 2</a></li>
+				<li><a href="Capitulo3.html">Capitulo 3</a></li>
+				<li><a href="Capitulo4.html">Capitulo 4</a></li>
+			</ul>
+		</nav>	
+	</div>
+	<!-- ---------------------------- -->
+    <section>
+        <div id="DivLogin">
+            <label>Usuario</label><input type="text">
+            <label>Contraseña</label><input type="password">
+            <div id="login"><input type="button" value="Login" ></div>
+        </div>
+    </section>
+
+
+
+    <footer></footer>
 </body>
 </html>
