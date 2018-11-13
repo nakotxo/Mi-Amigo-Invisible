5c1fbe3c05be1798f0d154ac965b406158e8aa5b
Jose Ignacio Hidalgo <HidalgoJ.Ignacio@gmail.com>
2018-11-13 14:24:29 +0100

    mejora css

--- Home.php
+++ Home.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Mi Amigo Invisible</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Mi Amigo Invisible</title>
 		<link rel="stylesheet" type="text/css" media="screen" href='http://<?=URLSERVIDOR?>/main.css' />
         <script src="main.js"></script>
     </head>
